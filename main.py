fa56599cc1a38eebe0128a0fefa50fd534abed19
Alan Noonan <aganoonan@gmail.com>
2020-02-27 11:26:11 +0000

    Fix formatting

--- main.py
+++ main.py
@@ -16,24 +16,24 @@ def main():
 
   for goodreads_book in get_goodreads_books():
     print("**************************************************")
-    print("Searching for '{}' by {}.".format(goodreads_book["title"], goodreads_book["author"]))
+    print(f"Searching for '{goodreads_book['title']}' by {goodreads_book['author']}.")
     print()
   
-    borrowbox_result_page = get_borrowbox_results(goodreads_book["title"], book_format)
+    borrowbox_result_page = get_borrowbox_results(goodreads_book['title'], book_format)
     borrowbox_result = parse_borrowbox_results(borrowbox_result_page, book_format)
 
     if borrowbox_result != "":
 
       # Check that the book title and the author name are similar to the Goodreads source, as Borrowbox will sometimes return a search result even if it's not what you are looking for
-      if compare(borrowbox_result["title"], goodreads_book["title"]) > 0.5 and compare(borrowbox_result["author"], goodreads_book["author"]) > 0.5:
-        print("Title:     {}".format(borrowbox_result["title"]))
-        print("Author:    {}".format(borrowbox_result["author"]))
+      if compare(borrowbox_result['title'], goodreads_book['title']) > 0.5 and compare(borrowbox_result['author'], goodreads_book['author']) > 0.5:
+        print(f"Title:     {borrowbox_result['title']}")
+        print(f"Author:    {borrowbox_result['author']}")
 
         if(book_format == "eAudiobooks"):
-          print("Narrator:  {}".format(borrowbox_result["narrator"]))
+          print(f"Narrator:  {borrowbox_result['narrator']}")
   
-        print("URL:       {}{}".format(base_url, borrowbox_result["url"]))
-        print("Available: {}".format(borrowbox_result["available"]))
+        print(f"URL:       {base_url}{borrowbox_result['url']}")
+        print(f"Available: {borrowbox_result['available']}")
       else:
         print("No results found.")
       
